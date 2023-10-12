e1b5a44421f5ac93e85086801783c194e6405546
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2023-10-12 16:02:51 +0600

    removed import

--- django-scrapy-integration/product/apis.py
+++ django-scrapy-integration/product/apis.py
@@ -1,25 +1,6 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from luluscraper.spiders.luluspider import LuluspiderSpider
-from django.http import HttpResponse
-from django.conf import settings
 import subprocess
+from django.conf import settings
 from django.http import JsonResponse
-
-def run_scrapy(request):
-    try:
-        process = CrawlerProcess(get_project_settings())
-        process.crawl(LuluspiderSpider)
-        process.start()
-    except Exception as e:
-        response_data = {
-            'error_message': str(e)
-        }
-        return JsonResponse(response_data)
-
-    # d = threads.deferToThread(run_scrapy)
-
-    return HttpResponse("Scrapy spider is running.")
 
 
 def run_scrapy_crawler(request):
--- django-scrapy-integration/product/urls.py
+++ django-scrapy-integration/product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .apis import run_scrapy_crawler, run_scrapy
+from .apis import run_scrapy_crawler
 
 urlpatterns = [
     path('lulu/', run_scrapy_crawler),

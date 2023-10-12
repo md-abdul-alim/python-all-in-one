d6665645a085383997d48539a755df80c85b5684
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2023-10-12 10:31:24 +0600

    lulu on the fly csv donwload

--- django-scrapy-integration/luluscraper/luluscraper/spiders/luluspider.py
+++ django-scrapy-integration/luluscraper/luluscraper/spiders/luluspider.py
@@ -12,3 +12,22 @@ class LuluspiderSpider(scrapy.Spider):
         for sub_category in sub_categories:
             relative_url = sub_category.css('div.col-lg-2 a').attrib['href']
             sub_category_url = "https://www.luluhypermarket.com/en-ae" + relative_url
+
+            yield response.follow(sub_category_url, callback=self.parse_product_page)
+
+    def parse_product_page(self, response):
+        products = response.css('div.product-img')
+
+        for product in products:
+            relative_url = product.css('a ::attr(href)').get()
+            product_url = "https://www.luluhypermarket.com" + relative_url
+
+            yield response.follow(product_url, callback=self.parse_product_details)
+
+    def parse_product_details(self, response):
+
+        yield {
+            'title': response.css('h1.product-name ::text').get(),
+            'price': response.css('div.price-tag div span span span small::text').get() + ' ' + response.css('div.price-tag div span span span::text').get(),
+        }
+
